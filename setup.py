90fbadeaed1d3f7c6991f44a45616da8188d4e94
Lumír Jasiok <lumir.jasiok@tieto.com>
2019-10-23 23:33:36 +0200

    Add LICENSE file, bumped version

--- setup.py
+++ setup.py
@@ -12,22 +12,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='hejtado-quido',
-    version='0.2.1',
+    version='0.2.2',
     description='Thermometer Quido RESTful API based on Flask-RESTPlus',
     url='https://github.com/hejtado/quido',
     author='Lumir Jasiok',
 
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     keywords='rest restful api flask swagger openapi flask-restplus',
