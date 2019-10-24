53ccc3daf11f8fe0f727e19e9bb931b33a60be29
Lumír Jasiok <lumir.jasiok@tieto.com>
2019-10-24 09:54:38 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='hejtado-quido',
-    version='0.2.3',
+    version='0.2.4',
     description='Thermometer Quido RESTful API based on Flask and Flask-RESTPlus',
     long_description=long_description,
     long_description_content_type="text/x-rst",
